ea832aac0ebe1854a6e620d07483bb3de2247235
Apus Industries <folorunsopraise580@gmail.com>
2023-02-20 17:04:37 +0100

    added dark and night mode and login form design

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,15 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import Footer from './components/Footer'
 import Header from './components/Header'
 
 const Layout = () => {
   const [day, setDay] = useState(false)
+  const [theme, setTheme] = useState('App')
+  
+  useEffect(()=>{
+    const userPrefrence = window.matchMedia && window.matchMedia('(prefers-color-scheme:dark)').matches
+    if (userPrefrence) {
+      setTheme('bg-dark text-light App')
+      setDay(true)
+    }
+  },[])
   const handledarkmode = () =>{
+    setTheme(theme === 'App' ? 'bg-dark text-light App':'App')
     setDay(!day)
+    
   }
   return (
-    <div className={day? 'bg-dark text-light App': ' App'}>
+    <div className={`app ${theme}`}>
       <Header handledarkmode={handledarkmode} day={day}/>
       <Outlet/>
       <Footer/>
